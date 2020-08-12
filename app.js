937dae1a3774bd97b6d7cacf95a0e79808d1cf34
erlansubekti <erlansubekti9@gmail.com>
2020-08-13 06:15:40 +0700

    up date 1

--- app.js
+++ app.js
@@ -34,4 +34,74 @@ app.post("/crud", async (req, res) => {
     }
 });
 
+app.get("/crud", async (req, res) => {
+    try {
+        const getAllUser = await User.findAll({})
+        res.json(getAllUser)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
+app.get("/crud/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const getUser = await User.findOne({
+            where: {
+                id: id
+            }
+        });
+
+        res.json(getUser)
+
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
+app.delete("/crud/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleteUser = await User.destroy({
+            where: {
+                id: id
+            }
+        });
+
+        await deleteUser;
+        res.json("berhasil dihapus");
+
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
+app.put("/crud/:id", async (req, res) => {
+    try {
+        const {
+            username,
+            email,
+            password
+        } = req.body
+        const id = req.params.id
+
+        const updateUser = await User.update({
+            username,
+            email,
+            password
+        }, {
+            where: {
+                id: id
+            }
+        });
+        await updateUser;
+        res.json("berhasil diupdate");
+    } catch (error) {
+        console.error(err.message);
+        res.status(500).send("server error");
+    }
+});
+
+
+
 app.listen(4500, () => console.log("port berjalan di 4500"));
